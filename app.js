23d90e6f05199c97104037ead2edeb116d0ed628
sheva-git <sh56781690@gmail.com>
2025-01-05 18:10:57 +0200

    up2

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const RENDER_API_URL = 'https://api.render.com/v1/services';
 
 app.use(cors());
 
-app.get('/services', async (req, res) => {
+app.get('/', async (req, res) => {
     try {
         const response = await axios.get(RENDER_API_URL, {
             headers: {
